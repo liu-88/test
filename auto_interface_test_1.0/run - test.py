454083a646ed37d558f83f204814f4623404a649
liuling <1194159281@qq.com>
2022-10-03 23:40:34 +0800

    修改报告生成方式

--- auto_interface_test_1.0/run - test.py
+++
@@ -1,7 +0,0 @@
-# -*-coding:utf-8-*-
-# @FileName :run.py.py
-# @Time     :2022-07-27 19:36
-# @Author   :Digua
-import pytest
-if __name__ == '__main__':
-    pytest.main()
--- auto_interface_test_1.0/run.py
+++ auto_interface_test_1.0/run.py
@@ -2,6 +2,10 @@
 # @FileName :run.py.py
 # @Time     :2022-07-27 19:36
 # @Author   :Digua
+
 import pytest
+import os
+
 if __name__ == '__main__':
     pytest.main()
+    os.system('allure generate ./temp -o ./report --clean')
