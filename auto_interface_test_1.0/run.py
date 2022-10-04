abf8b1db5dbb666d545bbc1af124c3379ee26cc9
liuling <1194159281@qq.com>
2022-10-04 22:53:10 +0800

    7

--- auto_interface_test_1.0/run.py
+++ auto_interface_test_1.0/run.py
@@ -8,4 +8,4 @@ import os
 
 if __name__ == '__main__':
     pytest.main()
-    os.system('allure generate ./results -o ./var/lib/jenkins/workspace/pythonAPI/report --clean')
+    os.system('allure generate /var/lib/jenkins/workspace/pythonAPI/results -o /var/lib/jenkins/workspace/pythonAPI/report --clean')
