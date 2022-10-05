71721f8a6dc5d3be257bdde19f18a2aa63caa09b
liuling <1194159281@qq.com>
2022-10-06 01:20:08 +0800

    6

--- auto_interface_test_1.0/run.py
+++ auto_interface_test_1.0/run.py
@@ -11,5 +11,6 @@ if __name__ == '__main__':
     pytest.main()
     if __name__ == "__main__":
         # 执行pytest单元测试，生成Allure报告需要的数据存在/allure-results目录
-        pytest.main()  # CASE_DIR 用例目录
-        os.system('allure generate ./allure-results -o ./allure-report --clean')
+        #pytest.main()  # CASE_DIR 用例目录
+        #os.system('allure generate ./allure-results -o ./allure-report --clean')
+        pytest.main(["-s", "-v", "--html=./reports/pytest.html", "--alluredir=./allure"])  # allure文件生成的目录
